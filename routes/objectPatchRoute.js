f67bb58974203e4a5bf033b56bd64be94b3e9090
odinaka-joy <odinakajoy909@gmail.com>
2020-10-24 20:59:00 +0100

    ft: users can download and resize images [Finishes 8]

--- routes/objectPatchRoute.js
+++ routes/objectPatchRoute.js
@@ -1,10 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-const { objectPatchValidationRules, validate } = require('../validations/objectPatchValidation');
 const isAuthenticated = require('../middlewares/isAuthenticated');
 const objectPatchController = require('../controllers/objectPatchController');
 
-router.patch('/', objectPatchValidationRules(), validate, isAuthenticated, objectPatchController.apply);
+router.patch('/', isAuthenticated, objectPatchController.apply);
 
 module.exports = router;
